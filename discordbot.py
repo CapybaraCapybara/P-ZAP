a842315e7e2f6e9a4d0b47809ceef218c431d40a
CapybaraCapybara <141387496+CapybaraCapybara@users.noreply.github.com>
2023-09-30 19:34:38 +0700

    <build>: ทำให้ bot แจ้งเตือนคนเข้าออก

--- discordbot.py
+++ discordbot.py
@@ -4,7 +4,22 @@ TOKEN = 'Insert Bot Token Here'
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True
 
 bot = discord.Client(intents=intents)
+
+@bot.event
+async def on_member_join(member):
+    guild = member.guild
+    for channel in guild.text_channels:
+        if channel and channel.permissions_for(guild.me).send_messages:
+            await channel.send(f"สวัสดีครับ {member.mention} ให้ผมนั่งคุยเป็นเพื่อนได้ไหมครับ")
+
+@bot.event
+async def on_member_remove(member):
+    guild = member.guild
+    for channel in guild.text_channels:
+        if channel and channel.permissions_for(guild.me).send_messages:
+            await channel.send(f"ขอบคุณที่ให้ผมได้นั่งคุยเป็นเพื่อน {member.name}")
 
 bot.run(TOKEN)
